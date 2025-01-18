f7fbdbd786ce7992dd4b0538b10601284252a6ca
Mohamed Abo Hebil <mabuhebil@gmail.com>
2025-01-18 17:05:12 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
 import Headar from "./componats/Header"
 import UserInput from "./componats/User_Input"
+import Results from "./componats/Rrsults"
 
 function App() {
+  const [userInput , setUserInput]= useState({
+    initialInvestment:10000,
+    annualInvestment:1200,
+    expectedReturn:6,
+    duration:10
+})
+function handelChange( inputIdentifier ,newValue){
+    setUserInput( prev => {
+        return{
+            ...prev,
+            [inputIdentifier]: +newValue
+        }
+    })
+}
   return (
-   
+   <>
     <Headar/>
-    
+    <UserInput userInput ={userInput} onChangeInput={handelChange}/>
+    <Results input ={userInput}/>
+   </>
   )
 }
 
